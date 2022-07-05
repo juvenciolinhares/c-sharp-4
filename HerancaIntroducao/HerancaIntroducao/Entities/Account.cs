0fc07362afa76f47471b63e95029a81a51a48ad8
juvenciolinhares <jlinhares@cpqi.com>
2022-07-05 16:26:14 -0300

    Herança Introducao

---
+++ HerancaIntroducao/HerancaIntroducao/Entities/Account.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace HerancaIntroducao.Entities
+{
+    internal class Account
+    {
+        public int Number { get;private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }//o protected permite acesso ao saldo pela subclasse
+
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
+
---
+++ HerancaIntroducao/HerancaIntroducao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HerancaIntroducao.Entities;
+
+namespace HerancaIntroducao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           BusinessAccount account = new BusinessAccount(8010, "Bob Brwon", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+           // account.Balance = 200.0; não consigo acessar de outra classe(diferente da subclasse) pq é protected
+        }
+    }
+}
