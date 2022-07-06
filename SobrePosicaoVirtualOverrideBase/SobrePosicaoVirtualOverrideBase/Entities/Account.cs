38c0b7c48db80b0172d2eb93b039ec5367675777
juvenciolinhares <jlinhares@cpqi.com>
2022-07-06 09:57:32 -0300

    SobrePosicao, Virtua,  Override, Base

---
+++ SobrePosicaoVirtualOverrideBase/SobrePosicaoVirtualOverrideBase/Entities/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SobrePosicaoVirtualOverrideBase.Entities
+{
+    internal class Account
+    {
+
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }//o protected permite acesso ao saldo pela subclasse
+
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //palavra virtual: permite sobrepor esse método nas subclasses
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.00;//taxa de saque
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
+/*SOBREPOSIÇÃO
+reimplemento na sublclasse uma operação da superclasse, mas posso fazer alterações*/
---
+++ SobrePosicaoVirtualOverrideBase/SobrePosicaoVirtualOverrideBase/Entities/SavingsAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SobrePosicaoVirtualOverrideBase.Entities
+{
+    internal class SavingsAccount : Account//heranca
+    {
+        public double InterestRate { get; set; }//taxa de juros
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        //sobrescrever a operação de saque, mas funcionando diferente pra essa subclasse:
+        public override void Withdraw(double amount)
+        {
+            //chamei o metodo da superclasse(base) e descontei mais 2 reais do saldo final.
+            base.Withdraw(amount);
+            Balance -= 2.00;
+        }
+    }
+}
+/*SOBREPOSIÇÃO
+reimplemento na sublclasse uma operação da superclasse, mas posso fazer alterações*/
