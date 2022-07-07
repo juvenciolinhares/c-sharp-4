acceffb7487d9d4273dc70cacf4bbcdf3597f7e8
juvenciolinhares <jlinhares@cpqi.com>
2022-07-07 14:53:12 -0300

    bloco Finally

---
+++ blocoFinally/blocoFinally/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace blocoFinally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally//o bloco finally executa, independe do resultado do trycatch
+            {
+                if(fs != null)
+                {
+                    fs.Close();// a função do finally aqui é finalizar o FileStream
+                }
+            }
+        }
+    }
+}
